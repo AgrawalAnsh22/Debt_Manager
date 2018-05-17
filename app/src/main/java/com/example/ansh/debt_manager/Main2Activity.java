bd66127a74a6a2138bafe5f18ee700ea13a83bcf
Ansh <500061120@stu.upes.ac.in>
2018-05-17 12:38:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ansh/debt_manager/Main2Activity.java
@@ -0,0 +1,32 @@
+package com.example.ansh.debt_manager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.jar.Attributes;
+
+public class Main2Activity extends AppCompatActivity {
+
+    EditText Name,Amount,Note;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Name = (EditText)findViewById(R.id.borrowName);
+        Amount= findViewById(R.id.borrowAmount);
+        Note = (EditText)findViewById(R.id.borrowNote);
+    }
+
+    public void cancelbtn(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/ansh/debt_manager/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.ansh.debt_manager;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void borrowbt(View view){
+        Intent i= new Intent(this,Main2Activity.class);
+        startActivity(i);
+
+    }
+    public void lendbtn(View v){
+        Intent intent= new Intent(this,Main3Activity.class);
+        startActivity(intent);
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
+
+
